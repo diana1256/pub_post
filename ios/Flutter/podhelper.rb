29aae66cfb57b51d5d03912cd511085c90a7d88e
Диана <146659691+diana1256@users.noreply.github.com>
2024-11-04 02:28:49 +0600

    Create podhelper.rb
    wwww

---
+++ ios/Flutter/podhelper.rb
@@ -0,0 +1,14 @@
+# Подключите все iOS поды для Flutter
+def flutter_install_all_ios_pods(flutter_application_path)
+  require File.join(flutter_application_path, '.ios', 'Flutter', 'flutter_export_environment')
+  # Укажите путь к другим подам Flutter
+  flutter_root = File.join(flutter_application_path, '.ios', 'Flutter')
+  # Добавление подов для всех плагинов
+  if defined?(Flutter)
+    pod 'Flutter', :path => flutter_root
+    pod 'FlutterPluginRegistrant', :path => File.join(flutter_root, 'FlutterPluginRegistrant')
+  end
+
+  # Укажите дополнительные поды, если это необходимо
+  # pod 'MyCustomPod', :path => 'path_to_my_custom_pod'
+end
