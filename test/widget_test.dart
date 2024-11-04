c11a8ca3586aa0a48baf288de5fe982cb046e80d
diana1256 <dcim3370@gmail.com>
2024-11-04 17:22:10 +0600

    nbhgfchhhh

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:pub_post/main.dart';
+import 'package:PostTime/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
